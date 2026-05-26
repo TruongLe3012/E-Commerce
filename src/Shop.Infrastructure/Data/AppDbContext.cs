0dfda0e2e6544e0dc53a439d499a280acc8d59fa
TruongLe3012 <ldt301203@gmail.com>
2026-05-26 16:31:29 +0700

    feat: setup app dbcontext and sql server connection

---
+++ src/Shop.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Shop.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(
+            DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
